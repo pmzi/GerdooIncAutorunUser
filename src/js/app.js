cc37373d1709d4902eff2141d2458ac8118c5d62
pmzi <pouya.mozafar@gmail.com>
2018-06-06 16:44:05 +0430

    Change of policy. New boilerplate has been generated.

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/../views/index.html`);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
