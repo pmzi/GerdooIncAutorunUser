cf5a862ca7efca123a0bcdddc4a9c3860c5c2f59
pmzi <pouya.mozafar@gmail.com>
2018-06-07 16:36:46 +0430

    a base model has been created

--- src/js/models/Model.js
+++ src/js/models/Model.js
@@ -1,19 +1,75 @@
 const Datastore = require('nedb');
 
-class Model{
+class Model {
 
-    loadDatabase(timestampData){
+    loadDatabase() {
 
         // let's load the db
         this.db = new Datastore({
-            filename: `${__dirname}/../../dbs/${this.dbName}.db`,
-            timestampData: timestampData || false
+            filename: `${__dirname}/../../db/${this.dbName}.db`
         });
 
         this.db.loadDatabase();
 
     }
 
+    /**
+     * Fetches all the DVDs
+     * @returns {Promise} - Returnes data in resolve of the Promise
+     */
+
+    fetchAll(sortProperty, sortType) {
+        let order = {};
+
+        order[sortProperty] = sortType;
+        
+        return new Promise((resolve, reject) => {
+            this.db.find({}).sort(order).exec((err,result)=>{
+                if(err === null){
+                    resolve(result);
+                }else{
+                    reject(err);
+                }
+            })
+        })
+
+    }
+
+    /**
+     * Deletes a record by it's ID
+     * @param {ObjectID} id - The id of the record
+     * @return {Promise}
+     */
+
+    deleteById(id) {
+        return new Promise((resolve, reject)=>{
+            this.db.remove({_id:id},(err)=>{
+                if(err === null){
+                    resolve();
+                }else{
+                    reject(err);
+                }
+            })
+        })
+    }
+
+    /**
+     * Gets the record by it's ID
+     * @param {ObjectID} id- The id of the record
+     * @return {Promise} - Returnes data in resolve of the Promise
+     */
+
+    getById(id) {
+        return new Promise((resolve, reject)=>{
+            this.db.findOne({_id:id},(err, result)=>{
+                if(err === null){
+                    resolve(result);
+                }else{
+                    reject(err);
+                }
+            })
+        })
+    }
 }
 
 module.exports = Model;
