88f0ee708acfe4462dc445344f3347a70135260d
pmzi <pouya.mozafar@gmail.com>
2018-06-18 23:37:07 +0430

    findClosest added to the software model

--- src/js/models/Software.js
+++ src/js/models/Software.js
@@ -34,6 +34,36 @@ class Software extends Model{
         })
     }
 
+    findClosest(string, catId, DVDNumber){
+        DVDNumber = parseInt(DVDNumber);
+        return new Promise((resolve, reject) => {
+
+            this.db.find({
+                $or:[
+                    {
+                        title: new RegExp(string,'i'),
+                        DVDNumber,
+                        cat: catId
+                    },
+                    {
+                        tags:{
+                            $regex: new RegExp(string,'i')
+                        },
+                        DVDNumber,
+                        cat: catId
+                    }
+                ]
+            }, (err, result) => {
+                if (err === null) {
+                    resolve(result)
+                } else {
+                    reject(err)
+                }
+            });
+
+        });
+    };
+
 }
 
 module.exports = Software;
