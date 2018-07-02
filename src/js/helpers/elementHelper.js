ae2a2c3b29b78887035b5a0f7afcdd4d30e13ebd
pmzi <pouya.mozafar@gmail.com>
2018-07-02 18:31:37 +0430

    elementHelper has been documented

--- src/js/helpers/elementHelper.js
+++ src/js/helpers/elementHelper.js
@@ -1,15 +1,21 @@
+// Some function helpers to speed up developement time
+
+// For selecting single element
 window.$ = (selector)=>{
     return document.querySelector(selector);
 };
 
+// For selecting multiple elements
 window.$$ = (selector)=>{
     return document.querySelectorAll(selector);
 };
 
+// For making an element empty
 Element.prototype.empty = function(){
     this.innerHTML = "";
 }
 
+// For appending an html inside another element
 Element.prototype.append = function(html){
         var div = document.createElement('div');
         div.innerHTML = html;
@@ -18,6 +24,7 @@ Element.prototype.append = function(html){
         }
 }
 
+// For triggering events
 Element.prototype.trigger = function(eventName){
     this.dispatchEvent(new Event(eventName));
 }
